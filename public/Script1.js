d629d44b9194eb5564bd907dd5aebffdc4aeb78d
Raul67531 <raul.sanchez.keob@gmail.com>
2026-03-25 12:24:22 +0100

    Update Script1.js

--- public/Script1.js
+++ public/Script1.js
@@ -4,7 +4,7 @@ import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.158/examples/js
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xdddddd);
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.set(0, 2, 5);
 
 const renderer = new THREE.WebGLRenderer({ canvas: document.getElementById("canvas") });
@@ -14,9 +14,9 @@ const light = new THREE.HemisphereLight(0xffffff, 0x444444);
 scene.add(light);
 
 const loader = new GLTFLoader();
-loader.load('./models/scene.glb', function(gltf) {
+loader.load('Clinica_MRI.glb', function (gltf) {
     scene.add(gltf.scene);
-}, undefined, function(error) {
+}, undefined, function (error) {
     console.error(error);
 });
 
@@ -24,4 +24,4 @@ function animate() {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
 }
-animate();// JavaScript source code
+animate();
