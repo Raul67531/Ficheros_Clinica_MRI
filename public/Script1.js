4c3d2d5c0710596d04639f902efaa4f14bc34795
Raul67531 <raul.sanchez.keob@gmail.com>
2026-03-25 15:55:20 +0100

    Update Script1.js

--- public/Script1.js
+++ public/Script1.js
@@ -1,1 +1,39 @@
+const scene = new THREE.Scene();
 
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
+camera.position.set(2, 2, 2);
+
+const renderer = new THREE.WebGLRenderer({
+    canvas: document.getElementById("canvas"),
+    antialias: true
+});
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+
+const light = new THREE.HemisphereLight(0xffffff, 0x444444, 1);
+scene.add(light);
+
+const loader = new THREE.GLTFLoader();
+loader.load('./Clinica_MRI.glb', function (gltf) {
+    scene.add(gltf.scene);
+    console.log("Modelo cargado correctamente");
+}, undefined, function (error) {
+    console.error("Error cargando GLB:", error);
+});
+
+function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+}
+animate();
+
+window.addEventListener('resize', () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+});
